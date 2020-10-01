908af902a05317fe2f3dabbbe34442900bd5ad3d
safin777 <sayedislamsafin7770555@gmail.com>
2020-10-01 23:24:39 +0600

    loop_elseif

---
+++ Loop_WhileLOOP_elseIF.py
@@ -0,0 +1,25 @@
+
+
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+x= input()
+n=int(x)
+
+if (n in range (6, 21) or n % 2 != 0):
+    print ("Weird")
+else:
+    print ("Not Weird")
+
+
+
+
+
+
+
