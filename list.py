7a17df65034fca03cc54eff8b76f8a6691e50acf
safin777 <sayedislamsafin7770555@gmail.com>
2020-11-27 00:38:04 +0600

    adding on list

---
+++ list.py
@@ -0,0 +1,21 @@
+
+#python access item from list
+arr=["apple","mango","orange"]
+
+print(arr[1])  #apple
+print(arr[0:3])#apple', 'mango', 'orange'
+print(arr[:3]) #apple', 'mango', 'orange'
+
+
+if arr[0]=="apple":
+  print("this is an apple ")
+else:
+  print ("prediction false")
+
+
+
+
+
+
+
+
