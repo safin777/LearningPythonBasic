e42adc7cc2437764a3bf023529d6c47fc24973b5
safin777 <sayedislamsafin7770555@gmail.com>
2021-01-07 00:39:39 +0600

    Lists

---
+++ Lists.py
@@ -0,0 +1,27 @@
+if __name__ == '__main__':
+    N = int(input())
+    listA=[]
+    for i in range (N):
+       command = list(input("").split(" "))\
+       
+       if command[0]=="insert":
+         a,b = map(int,(command[1],command[2]))
+         listA.insert(a,b)
+       
+       elif command[0]=="print":
+         print(listA)
+       
+       elif command[0]=="remove":
+         listA.remove(int(command[1]))
+       
+       elif command[0]=="append":
+            listA.append(int(command[1]))
+            
+       elif command[0]=="sort":
+            listA.sort()
+        
+       elif command[0]=="pop":
+            
+           listA.pop()
+       elif command[0]=="reverse":
+            listA.reverse()
